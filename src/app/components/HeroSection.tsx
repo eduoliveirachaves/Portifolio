1967a1af470785c0930ec38bad64cfc4a1106972
educhaves <eduardodeoliveirachaves7@gmail.com>
2025-03-30 15:46:23 -0300

    feat: animation

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
 
 export const HeroSection = () => {
   return (
@@ -6,18 +9,36 @@ export const HeroSection = () => {
       <div className="grid grid-cols-1 lg:grid-cols-12">
         <div className="col-span-7 place-self-center text-center sm:text-left">
           <h1 className="text-white mb-4 text:4xl sm:text-5xl  lg:text-6xl font-extrabold">
-            Hello, I&apos;m Edu
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-800 to-purple-500">
+              Hello, I&apos;m{" "}
+            </span>
+            <br />
+            <TypeAnimation
+              sequence={[
+                "Edu",
+                1000,
+                "Full Stack Developer",
+                1000,
+                "BackEnd Developer",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
             quos veritatis ex quo consequatur fugiat commodi numquam.
           </p>
           <div>
-            <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black">
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500  hover:bg-slate-200 text-white">
               Hire me
             </button>
-            <button className="px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3">
-              Download CV
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </span>
             </button>
           </div>
         </div>
