3e239f35abfa2cd3ba71ba52879a99cf66659742
educhaves <eduardodeoliveirachaves7@gmail.com>
2025-03-30 22:49:32 -0300

    feat: add project tag filter

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,78 +1,99 @@
+"use client";
+import React, { useState } from "react";
 import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
 
-const projectData = [
+const projectsData = [
   {
     id: 1,
-    title: "React Portifolio Website",
+    title: "React Portfolio Website",
     description: "Project 1 description",
     image: "/images/1.png",
     tag: ["All", "Web"],
-    git: "/",
     preview: "/",
+    git: "/",
   },
   {
     id: 2,
-    title: "Website",
+    title: "Potography Portfolio Website",
     description: "Project 2 description",
     image: "/images/2.png",
     tag: ["All", "Web"],
-    git: "/",
     preview: "/",
+    git: "/",
   },
   {
     id: 3,
-    title: "React",
+    title: "E-commerce Application",
     description: "Project 3 description",
     image: "/images/3.png",
     tag: ["All", "Web"],
-    git: "/",
     preview: "/",
+    git: "/",
   },
   {
     id: 4,
-    title: "Portifolio",
+    title: "Food Ordering Application",
     description: "Project 4 description",
     image: "/images/1.png",
-    tag: ["All", "Web"],
-    git: "/",
+    tag: ["All", "Mobile"],
     preview: "/",
+    git: "/",
   },
   {
     id: 5,
-    title: "React Web",
-    description: "Project 5 description",
+    title: "React Firebase Template",
+    description: "Authentication and CRUD operations",
     image: "/images/2.png",
     tag: ["All", "Web"],
-    git: "/",
     preview: "/",
+    git: "/",
   },
   {
     id: 6,
-    title: "React",
-    description: "Project 6 description",
+    title: "Full-stack Roadmap",
+    description: "Project 5 description",
     image: "/images/3.png",
     tag: ["All", "Web"],
-    git: "/",
     preview: "/",
+    git: "/",
   },
 ];
+const ProjectsSection = () => {
+  const [tag, setTag] = useState("All");
 
-const Projects = () => {
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag),
+  );
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
   return (
-    <div>
-      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12 ">
+    <>
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
         My Projects
       </h2>
-      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
-        <button className="rounded-full border-2 border-purple-500 px-6 py-3 text-xl cursor-pointer">
-          All
-        </button>
-        <button className="rounded-full border-2 border-slate-600 hover:border-white px-6 py-3 text-xl cursor-pointer">
-          Web
-        </button>
+      <div className="flex flex-row justify-center items-center gap-2 text-white my-6">
+        <ProjectTag
+          name="All"
+          onClick={handleTagChange}
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          name="Web"
+          onClick={handleTagChange}
+          isSelected={tag === "Web"}
+        />
+        <ProjectTag
+          name="Mobile"
+          onClick={handleTagChange}
+          isSelected={tag == "Mobile"}
+        />
       </div>
       <div className="grid md:grid-cols-3 gap-8 md:gap-12">
-        {projectData.map((project) => (
+        {filteredProjects.map((project) => (
           <ProjectCard
             key={project.id}
             title={project.title}
@@ -83,8 +104,8 @@ const Projects = () => {
           />
         ))}
       </div>
-    </div>
+    </>
   );
 };
 
-export default Projects;
+export default ProjectsSection;
